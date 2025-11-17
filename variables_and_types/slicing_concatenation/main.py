62dd5b672278f5413371c2e5557b4cb79e4e65ab
Compltb <>
2025-11-17 08:47:07 +0000

    Slicing & Concatenation

---
+++ variables_and_types/slicing_concatenation/main.py
@@ -0,0 +1,9 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = "milk"
+dairy2 = "cheese"
+bakery1 = "bread"
+sliced_item1 = dairy1[0:4]
+sliced_item2 = dairy2[0:6]
+sliced_item3 = bakery1[0:5]
+
+print("We have dairy and bakery items:", sliced_item1 + ", " + sliced_item2 + ", and " + sliced_item3 + " "  "in aisle 5")
