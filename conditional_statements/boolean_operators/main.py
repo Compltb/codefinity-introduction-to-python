15e37e1d49ccd2fec601482de83855c9080b7dbe
Compltb <>
2025-12-01 08:14:15 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,5 @@
+total_cost = 25.00
+discount_theshold = 20.00
+discountEligible = 22
+discountEligible = total_cost >= discount_theshold
+print("Is the purchase eligible for a discount?", discountEligible)
