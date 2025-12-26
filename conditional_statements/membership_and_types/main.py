eb36a08debb3d64dbf0e340304ebd48997378c6e
Compltb <>
2025-12-26 08:14:48 +0000

    Membership and Types

---
+++ conditional_statements/membership_and_types/main.py
@@ -0,0 +1,12 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+price_is_float = type(price) == float
+count_is_int = type(count) == int
+contains_raw = "raw" in description
+contains_Imported = "Imported" in description
+print("Contains 'raw':", contains_raw)
+print("Contains 'Imported':", contains_Imported)
+print("Is price a float?:", price_is_float)
+print("Is count an integer?:", count_is_int)
